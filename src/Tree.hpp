76499e90ed4f7dbc955ab1d4aafc850064bd44a2
Toshiaki Takada <toshiaki.takada@gmail.com>
2017-11-11 12:34:16 -0800

    Move Node under Tree.

--- src/Tree.hpp
+++ src/Tree.hpp
@@ -7,99 +7,103 @@ using namespace std;
 
 namespace librt {
 
-template<typename P, typename D>
-class Tree;
-
-template<typename P, typename D>
-class Node : public std::enable_shared_from_this<Node<P, D> >
-{
-public:
-  using NodePtr = shared_ptr<Node<P, D> >;
-
-  Node(Tree<P, D> *tree, const P& prefix)
-    : tree_(tree), parent_(nullptr), prefix_(prefix) {
-  }
-  Node(Tree<P, D> *tree, const P& prefix1, const P& prefix2)
-    : tree_(tree), parent_(nullptr), prefix_(prefix1, prefix2) {
-  }
-
-  const P& prefix() { return prefix_; }
-
-  //  const shared_ptr<Node<P, D> > child(u_char bit) {
-  const NodePtr child(u_char bit) {
-    return children_[bit];
-  }
-
-  void setChild(u_char bit, NodePtr node) {
-    children_[bit] = node;
-    node->parent_ = enable_shared_from_this<Node<P, D> >::shared_from_this();
-  }
-
-  void setParent(NodePtr parent) {
-    parent_ = parent;
-  }
-
-  const NodePtr nextNode() {
-    shared_ptr<Node<P, D> > curr =
-      enable_shared_from_this<Node<P, D> >::shared_from_this();
-
-    if (curr->children_[0]) {
-      return curr->children_[0];
-    }
-    if (curr->children_[1]) {
-      return curr->children_[1];
-    }
-
-    while (curr->parent_) {
-      if (curr->parent_->children_[0] == curr && curr->parent_->children_[1]) {
-        return curr->parent_->children_[1];
-      }
-      curr = curr->parent_;
-    }
-
-    return nullptr;
-  }
-
-private:
-  // Pointer to the tree.
-  Tree<P, D> *tree_;
-
-  // Pointer to parent node.
-  NodePtr parent_;
-
-  // Prefix of this node.
-  P prefix_;
-
-  // Children nodes.
-  NodePtr children_[2];
-
-  // Data.
-  shared_ptr<D> data_;
-};
-
+//
+// Radix Tree template
+//
 template<typename P, typename D>
 class Tree
 {
 public:
-  Tree() {}
-  ~Tree() {}
+  Tree()
+    : top_(), count_() { }
+  ~Tree() { }
 
-  shared_ptr<Node<P, D> > setNode(const P& prefix) {
-    return make_shared<Node<P, D> >(this, prefix);
+  //
+  // Tree Node
+  //
+  class Node : public std::enable_shared_from_this<Node>
+  {
+  public:
+    using NodePtr = shared_ptr<Node>;
+
+    Node(Tree *tree, const P& prefix)
+      : tree_(tree), parent_(nullptr), prefix_(prefix) {
+    }
+    Node(Tree *tree, const P& prefix1, const P& prefix2)
+      : tree_(tree), parent_(nullptr), prefix_(prefix1, prefix2) {
+    }
+
+    const P& prefix() { return prefix_; }
+
+    //  const shared_ptr<Node<P, D> > child(u_char bit) {
+    const NodePtr child(u_char bit) {
+      return children_[bit];
+    }
+
+    void setChild(u_char bit, NodePtr node) {
+      children_[bit] = node;
+      node->parent_ = enable_shared_from_this<Node>::shared_from_this();
+    }
+
+    void setParent(NodePtr parent) {
+      parent_ = parent;
+    }
+
+    const NodePtr nextNode() {
+      shared_ptr<Node> curr =
+        enable_shared_from_this<Node>::shared_from_this();
+
+      if (curr->children_[0]) {
+        return curr->children_[0];
+      }
+      if (curr->children_[1]) {
+        return curr->children_[1];
+      }
+
+      while (curr->parent_) {
+        if (curr->parent_->children_[0] == curr
+            && curr->parent_->children_[1]) {
+          return curr->parent_->children_[1];
+        }
+        curr = curr->parent_;
+      }
+
+      return nullptr;
+    }
+
+  private:
+    // Pointer to the tree.
+    Tree *tree_;
+
+    // Pointer to parent node.
+    NodePtr parent_;
+
+    // Prefix of this node.
+    P prefix_;
+
+    // Children nodes.
+    NodePtr children_[2];
+
+    // Data.
+    shared_ptr<D> data_;
+  };
+
+  shared_ptr<Node> setNode(const P& prefix) {
+    return make_shared<Node>(this, prefix);
   }
 
-  void setChildNode(shared_ptr<Node<P, D> > parent,
-                    shared_ptr<Node<P, D> > child) {
+  void setChildNode(shared_ptr<Node> parent,
+                    shared_ptr<Node> child) {
     u_char bit = (u_char)child->prefix().bit_at(parent->prefix().len());
     parent->setChild(bit, child);
     child->setParent(parent);
   }
 
   // Return a node if it exists, otherwise create one.
-  shared_ptr<Node<P, D> > getNode(const P& prefix) {
-    shared_ptr<Node<P, D> > new_node;
-    shared_ptr<Node<P, D> > curr = top_;
-    shared_ptr<Node<P, D> > matched = nullptr;
+  shared_ptr<Node> getNode(const P& prefix) {
+    shared_ptr<Node> new_node;
+    shared_ptr<Node> curr = top_;
+    shared_ptr<Node> matched = nullptr;
 
     while (curr
            && curr->prefix().len() <= prefix.len()
@@ -123,7 +127,7 @@ public:
       }
     }
     else {
-      new_node = make_shared<Node<P, D> >(this, curr->prefix(), prefix);
+      new_node = make_shared<Node>(this, curr->prefix(), prefix);
       setChildNode(new_node, curr);
 
       if (matched) {
@@ -146,15 +150,11 @@ public:
     return new_node;
   }
 
-  shared_ptr<Node<P, D>> AddNode(P p, D d) {
-
-  }
-
-  shared_ptr<Node<P, D> > top() { return top_; }
+  shared_ptr<Node> top() { return top_; }
 
 private:
   // top node of this tree.
-  shared_ptr<Node<P, D> > top_;
+  shared_ptr<Node> top_;
 
   // number of nodes in this tree.
   uint32_t count_;
